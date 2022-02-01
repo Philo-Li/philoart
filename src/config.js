29d1264d7297ea7e1a6b36390aa9b7bb9ed0704e
Philo <maerd.lll.p@gmail.com>
2022-02-01 21:18:21 +0800

    update aws config

--- src/config.js
+++ src/config.js
@@ -6,6 +6,10 @@ dotenv.config({ path: path.resolve(__dirname, '..', '/.env') });
 export default {
   port: process.env.REACT_APP_PORT,
   graphqlUri: process.env.REACT_APP_GRAPHQL_URI,
+  awsRegion: process.env.REACT_APP_AWS_REGION,
+  awsS3Bucket: process.env.REACT_APP_AWS_S3_BUCKET,
+  awsAccessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+  awsSecretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
   pexelApi: process.env.REACT_APP_PEXEL_API_KEY,
   pickyAdmin: process.env.REACT_APP_PICKY_ADMIN,
   visitorID: process.env.REACT_APP_PICKY_VISITOR,
