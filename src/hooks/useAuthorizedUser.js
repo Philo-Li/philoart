d029793596e5c2ad1503c3db9c8cc19b321940c8
Philo <maerd.lll.p@gmail.com>
2022-02-24 16:00:54 +0800

    remove unused variable from useAuthorizedUser hook

--- src/hooks/useAuthorizedUser.js
+++ src/hooks/useAuthorizedUser.js
@@ -2,47 +2,15 @@ import { useQuery } from '@apollo/client';
 
 import { GET_AUTHORIZED_USER } from '../graphql/queries';
 
-const useAuthorizedUser = (variables) => {
+const useAuthorizedUser = () => {
   const {
-    data, fetchMore, refetch, loading, ...result
+    data, loading, ...result
   } = useQuery(GET_AUTHORIZED_USER, {
     fetchPolicy: 'cache-and-network',
-    variables,
   });
-
-  const handleFetchMore = () => {
-    const canFetchMore = !loading && data && data.authorizedUser.likes.pageInfo.hasNextPage;
-
-    if (!canFetchMore) {
-      return;
-    }
-
-    fetchMore({
-      query: GET_AUTHORIZED_USER,
-      variables: {
-        after: data.authorizedUser.likes.pageInfo.endCursor,
-        ...variables,
-      },
-      updateQuery: (previousResult, { fetchMoreResult }) => {
-        const nextResult = {
-          authorizedUser: {
-            ...fetchMoreResult.authorizedUser,
-            edges: [
-              ...previousResult.authorizedUser.likes.edges,
-              ...fetchMoreResult.authorizedUser.likes.edges,
-            ],
-          },
-        };
-
-        return nextResult;
-      },
-    });
-  };
 
   return {
     authorizedUser: data ? data.authorizedUser : undefined,
-    fetchMore: handleFetchMore,
-    refetch,
     loading,
     ...result,
   };
