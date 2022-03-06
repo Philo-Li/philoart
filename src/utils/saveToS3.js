89d97229aa8e8a1359241cecee31cd8b3e4da76f
Philo <maerd.lll.p@gmail.com>
2022-03-06 22:49:13 +0800

    add saveToS3 utils

---
+++ src/utils/saveToS3.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import config from '../config';
+
+const baseUrl = config.philoartApi;
+
+const saveToS3 = async (photoId, buf) => {
+  const { url } = await axios.get(`${baseUrl}/s3Url/${photoId}`).then((res) => res.data);
+
+  // post the image direct to the s3 bucket
+  await fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'image/jpeg',
+    },
+    ContentEncoding: 'base64',
+    body: buf,
+  });
+
+  const imageUrl = url.split('?')[0];
+  return imageUrl;
+};
+
+export default saveToS3;
