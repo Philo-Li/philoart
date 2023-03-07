01c04c8bd9ecbe23ef6c44e7c563a6d240e197c6
Philo <maerd.lll.p@gmail.com>
2023-03-07 19:20:28 +0800

    update category cover

--- src/components/others/CategoryBar.js
+++ src/components/others/CategoryBar.js
@@ -3,6 +3,11 @@ import CategoryCard from './photo-card/CategoryCard';
 
 const CategoryBar = () => {
   const category = [{
+    id: 'YddPZXbgHkBRf4MVkDpXM',
+    title: 'Photograph',
+    cover: 'https://cdn.philoart.io/700x700/8/FS4MIHl-LbgSc25.jpg',
+  },
+  {
     id: 'eEWmzhJf3hR7E0NiJU98l',
     title: 'Paintings',
     cover: 'https://cdn.philoart.io/c/700x700/zu3VmDeCCM55iPp2zZAVZ.jpg',
@@ -11,11 +16,6 @@ const CategoryBar = () => {
     id: 'yibOLLFlIQtchyC6b5osL',
     title: 'Digital Art',
     cover: 'https://cdn.philoart.io/e/700x700/psYTmeZf1z6O2jukzPlyl.jpg',
-  },
-  {
-    id: 'YddPZXbgHkBRf4MVkDpXM',
-    title: 'Photograph',
-    cover: 'https://cdn.philoart.io/700x700/8/FS4MIHl-LbgSc25.jpg',
   }];
 
   return (
