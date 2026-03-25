16cfbbf9cb83ff8dc4955ebfdee354a1bce0e6a9
Philo Li <philo.li.official@gmail.com>
2026-03-25 15:51:24 +0900

    style: reduce photo grid gap to 6px

--- src/components/photo/PhotoGrid.tsx
+++ src/components/photo/PhotoGrid.tsx
@@ -39,7 +39,7 @@ export default function PhotoGrid({
   return (
     <div>
       {/* Masonry-like grid */}
-      <div className="columns-1 sm:columns-2 lg:columns-3 xl:columns-4 gap-3 space-y-3">
+      <div className="columns-1 sm:columns-2 lg:columns-3 xl:columns-4 gap-1.5 space-y-1.5">
         {photos.map((photo) => (
           <div key={photo.id} className="break-inside-avoid">
             <PhotoCard
