1c2510db2de47fee5f183e629b1adf8e9c83bb7d
Philo Li <philo.li.official@gmail.com>
2026-03-25 15:51:59 +0900

    style: add matching outer padding for equal grid spacing

--- src/components/photo/PhotoGrid.tsx
+++ src/components/photo/PhotoGrid.tsx
@@ -37,7 +37,7 @@ export default function PhotoGrid({
   }
 
   return (
-    <div>
+    <div className="p-1.5">
       {/* Masonry-like grid */}
       <div className="columns-1 sm:columns-2 lg:columns-3 xl:columns-4 gap-1.5 space-y-1.5">
         {photos.map((photo) => (
