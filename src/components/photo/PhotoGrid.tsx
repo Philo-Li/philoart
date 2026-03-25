cf9db57fff6379c15e0a1d9e534848e9264d3649
Philo Li <philo.li.official@gmail.com>
2026-03-25 16:07:50 +0900

    style: adjust grid spacing to 8px like Unsplash

--- src/components/photo/PhotoGrid.tsx
+++ src/components/photo/PhotoGrid.tsx
@@ -103,10 +103,10 @@ export default function PhotoGrid({
   }
 
   return (
-    <div className="p-1.5">
-      <div className="flex gap-1.5">
+    <div className="px-3 py-2">
+      <div className="flex gap-2">
         {columns.map((column, colIndex) => (
-          <div key={colIndex} className="flex-1 flex flex-col gap-1.5">
+          <div key={colIndex} className="flex-1 flex flex-col gap-2">
             {column.map((photo) => (
               <PhotoCard
                 key={photo.id}
